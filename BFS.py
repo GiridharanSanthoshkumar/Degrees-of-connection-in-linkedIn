be74709c081402512a3600e660ab173d230d8f5f
Giridharansanthoshkumar <giridharansanthosh0501@gmail.com>
2025-01-18 21:01:12 +0530

    modified the logic in Bidirectional_search

--- BFS.py
+++ BFS.py
@@ -12,7 +12,7 @@ social_graph= {
     'Aadhi': ['Bala', 'Aravinth', 'Vijay']
 }
 def distance_between_two_users(user1,user2):
-   
+    
     visited=set()
     queue = deque()
     queue.append(user1)
@@ -20,29 +20,49 @@ def distance_between_two_users(user1,user2):
     while(len(queue)>0):
         for i in range(len(queue)):
             node=queue.popleft()
-            print(node)
-            if(node in visited):
-                continue 
+            visited.add(node)
+            
             if(node==user2):
                 return count
             else:
                 #add current user connections to the queue
-
-                visited.add(node)
                 for i in range(len(social_graph[node])):
                     queue.append(social_graph[node][i])
         count=count+1
     return -1
 
 
-user1="Aravinth"
-user2="Karthi"
+user1="Thoufeeq"
+user2="Mega"
 result=distance_between_two_users(user1,user2)
 if result!=-1:
     print(f"number of hops between {user1} and {user2}:{result}")
 else:
     print("they are in different graphs")
 
+
+test_cases = [
+    ("Giri", "Sri"),  # 1 hop
+    ("Sri", "Mega"),  # 1 hop
+    ("Bala", "Aadhi"),  # 1 hop
+    ("Giri", "Mega"),  # 2 hops
+    ("Bala", "Karthi"),  # 2 hops
+    ("Aravinth", "Giri"),  # 2 hops
+    ("Iyap", "Mega"),  # 3 hops
+    ("Bala", "Iyap"),  # 3 hops
+    ("Karthi", "Aravinth"),  # 3 hops
+    ("Mega", "Iyap"),  # 4 hops
+    ("Aadhi", "Mega"),  # 4 hops
+    ("Sri", "Sri"),  # 0 hops
+    ("Thoufeeq", "Thoufeeq")  # 0 hops
+]
+
+for user1, user2 in test_cases:
+    no_of_hops = distance_between_two_users(user1, user2)
+    if(no_of_hops==-1):
+         print("They are not in the same graph")
+         continue
+    print(f"No of hops to reach from {user1} to {user2}: {no_of_hops}")
     
     
 
--- Bidirectional_BFS.py
+++ Bidirectional_BFS.py
@@ -1,17 +1,3 @@
-# do BFS from the source node and target node
-
-#two queue and two visited list
-
-#initially ,put the start node to the visited1 and expand its neighbors into the queue1,do the same for 
-#target as well.
-
-
-#do a level traversal for source and do a level traversal for target node
-
-#at the time of visiting a node(pop out from the queue),we ll expand
-#to expand->the current node should not be in visited list(to avoid the cycle in the graph)
-
-#check the current node already in the opposite visited list,if so then here is the intersection and stop
 
 from collections import deque 
 
@@ -28,7 +14,7 @@ social_graph= {
     'Aadhi': ['Bala', 'Aravinth', 'Vijay']
 }
 
-
+          
 def bidir_search(source,target):
     if source == target:
         return 0  
@@ -47,8 +33,9 @@ def bidir_search(source,target):
         
         for i in range(len(queue1)):
             cur_node=queue1.popleft()
+
             if(cur_node in visited2):
-                    return levelcount1+levelcount2
+                    return  (levelcount1 + levelcount2)
             if(cur_node not in visited1):
                     visited1.add(cur_node)
                     for node in social_graph[cur_node]:
@@ -66,23 +53,35 @@ def bidir_search(source,target):
                     for node in social_graph[cur_node]:
                         if(node not in visited2):  
                             queue2.append(node)
-
+        
         levelcount2+=1
     
 
     return -1
     
-    
-    
-user1="Aravinth"
-user2="Karthi"
-no_of_hops=bidir_search(user1,user2)    
-if(no_of_hops==-1):
-    print("Two users are not having any connections,they are in the different graphs")
-else:
-    print(f"No of hops to reach the {user1} to {user2} is: {no_of_hops-1}")
+ 
+test_cases = [
+    ("Giri", "Sri"),  # 1 hop
+    ("Sri", "Mega"),  # 1 hop
+    ("Bala", "Aadhi"),  # 1 hop
+    ("Giri", "Mega"),  # 2 hops
+    ("Bala", "Karthi"),  # 2 hops
+    ("Aravinth", "Giri"),  # 2 hops
+    ("Iyap", "Mega"),  # 3 hops
+    ("Bala", "Iyap"),  # 3 hops
+    ("Karthi", "Aravinth"),  # 3 hops
+    ("Mega", "Iyap"),  # 4 hops
+    ("Aadhi", "Mega"),  # 4 hops
+    ("Sri", "Sri"),  # 0 hops
+    ("Thoufeeq", "Thoufeeq")  # 0 hops
+]
 
-        
+for user1, user2 in test_cases:
+    no_of_hops = bidir_search(user1, user2)
+    if(no_of_hops==-1):
+         print("They are not in the same graph")
+    print(f"No of hops to reach from {user1} to {user2}: {no_of_hops-1}")
+  
                     
 
 
